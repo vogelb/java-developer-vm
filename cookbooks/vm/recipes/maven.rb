09c91f5586596b84ed30b687e8cf7523aad521da
Benno Vogel <mail@benno-vogel.de>
2016-10-29 01:47:39 +0200

    Update Maven Version

--- cookbooks/vm/recipes/maven.rb
+++ cookbooks/vm/recipes/maven.rb
@@ -2,8 +2,8 @@
 # install maven
 node.set['maven']['version'] = '3'
 node.set['maven']['install_java'] = false
-node.set['maven']['3']['version'] = '3.1.1'
-node.set['maven']['3']['url'] = 'http://apache.mirrors.tds.net/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz'
-node.set['maven']['3']['checksum'] = '077ed466455991d5abb4748a1d022e2d2a54dc4d557c723ecbacdc857c61d51b'
+node.set['maven']['3']['version'] = '3.3.9'
+node.set['maven']['3']['url'] = 'http://apache.mirrors.tds.net/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz'
+node.set['maven']['3']['checksum'] = '516923b3955b6035ba6b0a5b031fbd8b'
 
 include_recipe 'maven'
