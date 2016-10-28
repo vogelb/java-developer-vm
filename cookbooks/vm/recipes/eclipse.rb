c97f68b9ed8177189e31f166464b5b033aa13c5a
Benno Vogel <mail@benno-vogel.de>
2016-10-29 01:38:31 +0200

    Update eclipse version

--- cookbooks/vm/recipes/eclipse.rb
+++ cookbooks/vm/recipes/eclipse.rb
@@ -2,7 +2,8 @@
 # install eclipse
 node.set['eclipse']['version'] = 'mars'
 # use the URL directly so it can be better cached (-> etag)
-node.set['eclipse']['url'] = 'http://ftp.heanet.ie/pub/eclipse/technology/epp/downloads/release/mars/R/eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz'
+# node.set['eclipse']['url'] = 'http://ftp.heanet.ie/pub/eclipse/technology/epp/downloads/release/mars/R/eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz'
+node.set['eclipse']['url'] = 'http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/neon/R/eclipse-java-neon-R-linux-gtk-x86_64.tar.gz'
 # don't install any plugins yet
 node.set['eclipse']['plugins'] = []
 
